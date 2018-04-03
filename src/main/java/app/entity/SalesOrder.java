e1d1856667c90b47f519a14acf0a86907b70e70c
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-04-03 11:32:53 +0000

    First commit...

---
+++ src/main/java/app/entity/SalesOrder.java
@@ -0,0 +1,169 @@
+package app.entity;
+
+import java.io.*;
+import javax.persistence.*;
+import java.util.*;
+import javax.xml.bind.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import cronapi.rest.security.CronappSecurity;
+
+
+/**
+ * Classe que representa a tabela SALESORDER
+ * @generated
+ */
+@Entity
+@Table(name = "\"SALESORDER\"")
+@XmlRootElement
+@CronappSecurity
+@JsonFilter("app.entity.SalesOrder")
+public class SalesOrder implements Serializable {
+
+  /**
+   * UID da classe, necessário na serialização
+   * @generated
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @generated
+   */
+  @Id
+  @Column(name = "id", nullable = false, insertable=true, updatable=true)
+  private java.lang.String id = UUID.randomUUID().toString().toUpperCase();
+
+  /**
+  * @generated
+  */
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "addedDate", nullable = true, unique = false, insertable=true, updatable=true)
+  
+  private java.util.Date addedDate;
+
+  /**
+  * @generated
+  */
+  @Column(name = "fiscalDocument", nullable = true, unique = false, insertable=true, updatable=true)
+  
+  private java.lang.String fiscalDocument;
+
+  /**
+  * @generated
+  */
+  @ManyToOne
+  @JoinColumn(name="fk_orderStatus", nullable = true, referencedColumnName = "id", insertable=true, updatable=true)
+  
+  private OrderStatus orderStatus;
+
+  /**
+   * Construtor
+   * @generated
+   */
+  public SalesOrder(){
+  }
+
+
+  /**
+   * Obtém id
+   * return id
+   * @generated
+   */
+  
+  public java.lang.String getId(){
+    return this.id;
+  }
+
+  /**
+   * Define id
+   * @param id id
+   * @generated
+   */
+  public SalesOrder setId(java.lang.String id){
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Obtém addedDate
+   * return addedDate
+   * @generated
+   */
+  
+  public java.util.Date getAddedDate(){
+    return this.addedDate;
+  }
+
+  /**
+   * Define addedDate
+   * @param addedDate addedDate
+   * @generated
+   */
+  public SalesOrder setAddedDate(java.util.Date addedDate){
+    this.addedDate = addedDate;
+    return this;
+  }
+
+  /**
+   * Obtém fiscalDocument
+   * return fiscalDocument
+   * @generated
+   */
+  
+  public java.lang.String getFiscalDocument(){
+    return this.fiscalDocument;
+  }
+
+  /**
+   * Define fiscalDocument
+   * @param fiscalDocument fiscalDocument
+   * @generated
+   */
+  public SalesOrder setFiscalDocument(java.lang.String fiscalDocument){
+    this.fiscalDocument = fiscalDocument;
+    return this;
+  }
+
+  /**
+   * Obtém orderStatus
+   * return orderStatus
+   * @generated
+   */
+  
+  public OrderStatus getOrderStatus(){
+    return this.orderStatus;
+  }
+
+  /**
+   * Define orderStatus
+   * @param orderStatus orderStatus
+   * @generated
+   */
+  public SalesOrder setOrderStatus(OrderStatus orderStatus){
+    this.orderStatus = orderStatus;
+    return this;
+  }
+
+  /**
+   * @generated
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    SalesOrder object = (SalesOrder)obj;
+    if (id != null ? !id.equals(object.id) : object.id != null) return false;
+    return true;
+  }
+
+  /**
+   * @generated
+   */
+  @Override
+  public int hashCode() {
+    int result = 1;
+    result = 31 * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+}
